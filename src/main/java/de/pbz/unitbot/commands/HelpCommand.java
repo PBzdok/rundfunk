3d4e44f19a30aa7670a886f415ce0fc267e36470
Philipp Bzdok <philipp.bzdok@mailbox.org>
2020-04-25 17:05:13 +0200

    Added HelpCommand

---
+++ src/main/java/de/pbz/unitbot/commands/HelpCommand.java
@@ -0,0 +1,26 @@
+package de.pbz.unitbot.commands;
+
+import de.pbz.unitbot.Command;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public class HelpCommand implements Command {
+    private static final Logger LOG = LoggerFactory.getLogger(HelpCommand.class);
+
+    private static final String HELP = "`!cat` -> Just cats...\n" +
+            "`!ttt` -> Get the G-Unit TTT server address.\n" +
+            "`!mc` -> Get the G-Unit MC server address.";
+
+    @Override
+    public Mono<Void> execute(MessageCreateEvent event) {
+        LOG.info("Handling !help message.");
+        return Mono.just(event)
+                .map(MessageCreateEvent::getMessage)
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage(HELP))
+                .then();
+    }
+}
