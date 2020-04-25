ef3f9ba28243f45cfdfd6c36d32a3ffd51b92ae2
Philipp Bzdok <philipp.bzdok@mailbox.org>
2020-04-25 15:21:26 +0200

    Removed unused Flux and command check

--- src/main/java/de/pbz/unitbot/commands/MCCommand.java
+++ src/main/java/de/pbz/unitbot/commands/MCCommand.java
@@ -5,21 +5,18 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MCCommand implements Command {
     private static final Logger LOG = LoggerFactory.getLogger(MCCommand.class);
 
-    private static final String MC_COMMAND = "!mc";
     private static final String MC_ADDRESS = "`bte.mcs.lol`";
 
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
         LOG.info("Handling !mc message.");
-        return Flux.just(event)
+        return Mono.just(event)
                 .map(MessageCreateEvent::getMessage)
-                .filter(m -> m.getContent().orElse("").equalsIgnoreCase(MC_COMMAND))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage(MC_ADDRESS))
                 .then();
--- src/main/java/de/pbz/unitbot/commands/TTTCommand.java
+++ src/main/java/de/pbz/unitbot/commands/TTTCommand.java
@@ -5,21 +5,18 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class TTTCommand implements Command {
     private static final Logger LOG = LoggerFactory.getLogger(TTTCommand.class);
 
-    private static final String TTT_COMMAND = "!ttt";
     private static final String TTT_ADDRESS = "`connect h2879589.stratoserver.net:27015; password <enter_password>`";
 
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
         LOG.info("Handling !ttt message.");
-        return Flux.just(event)
+        return Mono.just(event)
                 .map(MessageCreateEvent::getMessage)
-                .filter(m -> m.getContent().orElse("").equalsIgnoreCase(TTT_COMMAND))
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage(TTT_ADDRESS))
                 .then();
