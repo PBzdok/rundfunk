d97a89d63310f2cc51fa562eeb3668070a433de1
Philipp Bzdok <philipp.bzdok@mailbox.org>
2020-12-24 14:20:11 +0100

    improved help

--- src/main/java/de/pbz/rundfunk/commands/HelpCommand.java
+++ src/main/java/de/pbz/rundfunk/commands/HelpCommand.java
@@ -2,6 +2,7 @@ package de.pbz.rundfunk.commands;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.rest.util.Color;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
@@ -9,24 +10,27 @@ import reactor.core.publisher.Mono;
 public class HelpCommand implements Command {
     private static final Logger LOG = LoggerFactory.getLogger(HelpCommand.class);
 
-    private static final String HELP = "`!cat` -> Just cats...\n" +
-            "`!wt` -> Get WatchTogether G-Unit room url.\n" +
-            "`!rps` -> Play rock-paper-scissors.\n" +
-            "`!join` -> Make the bot join the voice channel you are in. Permissions still apply!\n" +
-            "`!play <direct_link>` -> Play single audio (youtube, soundcloud, bandcamp, vimeo) in the voice channel the bot is in.\n" +
-            "`!playlist <direct_link>` -> Play playlist (youtube, soundcloud, bandcamp, vimeo) in the voice channel the bot is in.\n" +
-            "`!track` -> Show currently playing track information.\n" +
-            "`!skip` -> Skip current track if there are more queued tracks.\n" +
-            "`!df` -> Play epic DarkForce skit\n." +
-            "`!wimbledon` -> I don't know why we have this...";
-
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
         LOG.info("Handling !help message.");
         return Mono.just(event)
                 .map(MessageCreateEvent::getMessage)
                 .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage(HELP))
+                .flatMap(channel ->
+                        channel.createEmbed(spec ->
+                                spec.setColor(Color.DEEP_SEA)
+                                        .setTitle("Ein Songwunsch gefällig?")
+                                        .addField("`!cat`", "Just cats...", false)
+                                        .addField("`!wt`", "Create WatchTogether Room...", false)
+                                        .addField("`!rps`", "Play Rock-Paper-Scissors.", false)
+                                        .addField("`!join`", "Let the bot join your voice channel.", false)
+                                        .addField("`!play <direct_link>`", "Play single audio (youtube, soundcloud, bandcamp, vimeo).", false)
+                                        .addField("`!playlist <direct_link>`", "Play playlist (youtube, soundcloud, bandcamp, vimeo).", false)
+                                        .addField("`!track`", "Show currently playing track information.", false)
+                                        .addField("`!skip`", "Skip current track if more are queued", false)
+                                        .addField("`!df`", "Play DarkForce skit", false)
+                                        .addField("`!wimbledon`", "Don't ask....", false)
+                        ))
                 .then();
     }
 }
